5aceb0e9f84da61dda4a7b18663d8d714fcdd038
Amine-Bslh <medaminebslh@gmail.com>
2022-02-24 18:28:33 +0100

    IOC Methode

---
+++ src/com/spring/Football.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+public class Football implements Sport{
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		System.out.println("Start Football !");
+	}
+
+}
